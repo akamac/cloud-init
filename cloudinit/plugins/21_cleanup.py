925138cfe62d07b14f05cfa0c63206499cfaf4dc
Alexey Miasoedov <amiasoedov@intermedia.net>
2018-02-15 11:45:38 +0300

    more cleanup

--- cloudinit/plugins/21_cleanup.py
+++ cloudinit/plugins/21_cleanup.py
@@ -8,6 +8,8 @@ import glob
 if cloud_config.get('Cleanup'):
     print('Cleaning up bash history and temp files')
     os.remove('/root/.bash_history')
+    os.remove('/var/log/auth.log')
     map(os.remove, glob.glob('/root/.ssh/*'))
     map(os.remove, glob.glob('/tmp/*'))
-    map(os.remove, glob.glob('/vat/tmp/*'))
+    map(os.remove, glob.glob('/var/tmp/*'))
+    map(os.remove, glob.glob('/var/log/installer/*'))
